70ee2f55ccbe990a9d2eda62c23bd0cf6a2e3bf0
Steven Kester Yuwono <yulonglong2005@yahoo.com>
2017-10-20 16:18:04 +0800

    Added command 3 and 4 (poly and poly2 dataset)

---
+++ command/4.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+# First argument is gpu number
+# Second argument is gpu name
+
+if [ -z "$1" ]
+    then
+        echo "Please enter gpu number as first argument!"
+        exit 1
+fi
+
+if [ -z "$2" ]
+    then
+        echo "Please enter gpu name as second argument"
+        exit 1
+fi
+
+gpu_num=$1
+gpu_name=$2
+theano_flags_device=gpu${gpu_num}
+
+# Check whether gpu_name contains nscc
+# If yes, do not specify GPU number in THEANO_FLAGS
+# If no, specify GPU number in THEANO_FLAGS
+
+if [[ $gpu_name == *"nscc"* ]]
+    then
+        theano_flags_device=gpu
+fi
+
+echo "Running script on ${theano_flags_device} : ${gpu_name}"
+
+expt_num="04"
+function_name="polynomial2"
+num_parameter="2"
+num_layer="2"
+test_limit="10000"
+train_limit="100000"
+optimizer="adagrad"
+
+for rand in {1..5}
+do
+    THEANO_FLAGS="device=${theano_flags_device},floatX=float32,mode=FAST_RUN" python train.py \
+    -tr data/${function_name}_10m.out -dt ${function_name} -o expt${expt_num}-${function_name}-a${optimizer}-l${num_layer}-${rand}${gpu_name} \
+    -t nn --epochs 50 -a ${optimizer} -l ${num_layer} \
+    --active-sampling-batch 256 --active-sampling-minimum 1000 \
+    --num-parameter ${num_parameter} \
+    --train-amount-limit ${train_limit} --test-amount-limit ${test_limit} --test-size 2000
+done

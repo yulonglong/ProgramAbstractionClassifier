aa6d95caa4ae3c98d843846e3fda3f6d0dc8ef0e
Steven Kester Yuwono <yulonglong2005@yahoo.com>
2017-11-18 21:55:13 +0800

    Added two run train command and setup command

---
+++ setup.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# If you are running on a fresh computer, please run this setup files to install all the python packages
+# This assumes pip is already installed
+
+pip install numpy --user
+pip install theano==0.8.2 --user
+pip install keras==1.1.1 --user
+pip install nltk==3.2.2 --user
+pip install pydot==1.1.0 --user
+pip install h5py==2.6.0 --user
+pip install matplotlib==1.5.3 --user
+
